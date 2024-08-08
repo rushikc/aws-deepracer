f71a79e48d98f1adb2af1bb4ec37c18a3b77d996
rushikc <rushi743@gmail.com>
2024-08-08 18:13:37 +0530

    Added axle length & higher speed

--- good_car.py
+++ good_car.py
@@ -6,32 +6,40 @@ def reward_function(params):
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    car_axile_length = 0.165
     
     reward = 1e-3
+    lowest_reward = 1e-3
+    highest_reward = 2.0
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    track_width = track_width + (car_axile_length*0.5)
+    marker_3 = 0.5 * track_width
     
-    # Give higher reward if the car is closer to center line and vice versa
+    # Give higher reward if the car is within the track
     if distance_from_center <= marker_1:
-        reward += 2.0
-    elif distance_from_center <= marker_2:
-        reward += 0.5
-    elif distance_from_center <= marker_3:
-        reward += 0.1
+        reward += highest_reward
     else:
-        return float(reward) # mostly out of track
+        return float(reward) # car is out of track
     
     steering_angle = params['steering_angle']
     speed = params['speed']
     
     if -5 < steering_angle < 5 :
-        if speed > 2.5:
-            reward += 2.0
-        elif speed > 2.0:
-            reward += 1.0
-    
+        if speed == 4:
+            reward += highest_reward
+        elif speed > 3.8:
+            reward += (highest_reward - 0.1)
+        elif speed > 3.6:
+            reward += (highest_reward - 0.2)
+        elif speed > 3.4:
+            reward += (highest_reward - 0.3)
+        elif speed > 3.2:
+            reward += (highest_reward - 0.4)
+        elif speed > 3:
+            reward += (highest_reward - 0.5)
+        elif speed > 2.8:
+            reward += (highest_reward - 0.6)
     
     
     return float(reward)
